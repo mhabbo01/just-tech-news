12b8950c70221428c610af4bd5d037b0fa30579e
Matt Habbo <matthabbo45@gmail.com>
2022-04-28 16:06:39 -0400

    added Post routes for CRUD

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//create associations
+User.hasMany(Post, {  //defining relationship of the User model to the Post
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {  // defining relationship of the Post model to the User
+    foreignKey: 'user_id'
+});
+
 
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 router.use('/users', userRoutes); // we take the endpoints and prefix them with the path of /users
+router.use('/posts', postRoutes);
 
 module.exports = router;
