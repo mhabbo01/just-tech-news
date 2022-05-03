cf6f3f14c0d6a23aada3e680d846cb2bc8b49876
Matt Habbo <matthabbo45@gmail.com>
2022-05-03 16:54:45 -0400

    added stylesheet, handlebars layouts, updated routes folder to controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,17 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes); // routes to the homepage.handlebars
+
 router.use('/api', apiRoutes); // collecting the packaged group of API endpoints and prefixing them with the path /api
 
+//this needs to be last!
 router.use((req, res) => { // if we make a request to any endpoint that doesnt exist, we will receive a 404 error
     res.status(404).end();
 });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const routes = require('./routes'); // collected all the routes and packaged them up
+const routes = require('./controllers/'); // collected all the routes and packaged them up
 const sequelize = require('./config/connection'); //importing the connection to Sequelize from here
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //turn on routes
 app.use(routes);
