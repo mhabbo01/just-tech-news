e191e3b526975b3617274b1f44f0a71b2f3265d9
Matt Habbo <matthabbo45@gmail.com>
2022-05-03 15:48:51 -0400

    added comment model and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes); // we take the endpoints and prefix them with the path of /users
 router.use('/posts', postRoutes);
+router.use('./comments', commentRoutes);
 
 module.exports = router;
